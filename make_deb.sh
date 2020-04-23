bd258af87cd6d934cce6de27c6144317cc12c441
Baran Sekin <baransekin@gmail.com>
2020-04-24 00:13:35 +0300

    Removed the desktop app from the deb package

--- make_deb.sh
+++ make_deb.sh
@@ -9,9 +9,6 @@ DEBIAN_PATH=$TMP_DIR/passwall-debian
 PASSWALL_OPT_PATH=$DEBIAN_PATH/usr/share/passwall
 SERVER_BIN_PATH=$DEBIAN_PATH/usr/bin/passwall/server
 WEB_BIN_PATH=$PASSWALL_OPT_PATH/public
-DESKTOP_BIN_PATH=$DEBIAN_PATH/usr/bin/passwall/client
-APPLICATION_PATH=$DEBIAN_PATH/usr/share/applications
-ICON_PATH=$DEBIAN_PATH/usr/share/pixmaps
 
 rm -rf $TMP_DIR
 
@@ -20,19 +17,13 @@ mkdir $DEBIAN_PATH
 mkdir -p $PASSWALL_OPT_PATH
 mkdir -p $SERVER_BIN_PATH
 mkdir -p $WEB_BIN_PATH
-mkdir -p $DESKTOP_BIN_PATH
-mkdir -p $APPLICATION_PATH
-mkdir -p $ICON_PATH
 
 mkdir -p $DEBIAN_PATH/DEBIAN
 cp -r ./debian/control $DEBIAN_PATH/DEBIAN/control
 echo "Version: 000.$1" >> $DEBIAN_PATH/DEBIAN/control
 cp -r ./debian/postinst $DEBIAN_PATH/DEBIAN/postinst
-cp ./debian/passwall.desktop $APPLICATION_PATH/passwall.desktop
-cp ./debian/passwall.png $ICON_PATH/passwall.png
 
 $GIT clone https://github.com/pass-wall/passwall-server $SERVER_PATH
-$GIT clone https://github.com/pass-wall/passwall-desktop $DESKTOP_PATH
 $GIT clone https://github.com/pass-wall/passwall-web $WEB_PATH
 
 echo "Building passwall-server..."
@@ -46,11 +37,6 @@ sed -i "s/next build/next build \&\& next export/g" $WEB_PATH/package.json
 cd $WEB_PATH && $NPM install && $NPM run build
 cp -r $WEB_PATH/out/* $WEB_BIN_PATH/
 echo "Building passwall-web completed successfully."
-
-echo "Building passwall-desktop..."
-cd $DESKTOP_PATH && $NPM install && $NPM run build:linux
-cp -r $DESKTOP_PATH/build/PassWall-linux-x64/* $DESKTOP_BIN_PATH/
-echo "Building passwall-desktop completed successfully."
 
 echo "Building deb package..."
 dpkg-deb --build $DEBIAN_PATH
