11d39e529fc8ac198046f9f6000575ca119b1a10
Baran Sekin <baransekin@gmail.com>
2020-04-23 11:06:31 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# passwall-debian
+# Building Debian Package
+
+Install the dependencies 
+
+```
+sudo apt install git npm
+```
+
+Clone the repository
+
+```
+git clone https://github.com/brnskn/passwall-debian.git
+
+cd passwall-debian
+```
+
+Install GOLANG
+
+```
+wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.14.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+```
+
+Run the shell script
+
+```
+chmod +x make_deb.sh
+./make_deb.sh
+```
+
+Install the deb package
+
+```
+sudo apt install /tmp/passwall-build/passwall-debian.deb
+```
---
+++ make_deb.sh
@@ -0,0 +1,57 @@
+GIT=$(which git)
+NPM=$(which npm)
+GO=$(which go)
+TMP_DIR=/tmp/passwall-build
+SERVER_PATH=$TMP_DIR/passwall-server
+DESKTOP_PATH=$TMP_DIR/passwall-desktop
+WEB_PATH=$TMP_DIR/passwall-web
+DEBIAN_PATH=$TMP_DIR/passwall-debian
+SERVER_BIN_PATH=$DEBIAN_PATH/usr/bin/passwall/server
+WEB_BIN_PATH=$SERVER_BIN_PATH/public
+DESKTOP_BIN_PATH=$DEBIAN_PATH/usr/bin/passwall/client
+APPLICATION_PATH=$DEBIAN_PATH/usr/share/applications
+ICON_PATH=$DEBIAN_PATH/usr/share/pixmaps
+
+rm -rf $TMP_DIR
+
+mkdir $TMP_DIR
+mkdir $DEBIAN_PATH
+mkdir -p $SERVER_BIN_PATH
+mkdir -p $WEB_BIN_PATH
+mkdir -p $DESKTOP_BIN_PATH
+mkdir -p $APPLICATION_PATH
+mkdir -p $ICON_PATH
+
+mkdir -p $DEBIAN_PATH/DEBIAN
+cp -r ./debian/control $DEBIAN_PATH/DEBIAN/control
+echo "Version: 1.$1" >> $DEBIAN_PATH/DEBIAN/control
+cp -r ./debian/postinst $DEBIAN_PATH/DEBIAN/postinst
+cp ./debian/passwall.desktop $APPLICATION_PATH/passwall.desktop
+cp ./debian/passwall.png $ICON_PATH/passwall.png
+
+$GIT clone https://github.com/pass-wall/passwall-server $SERVER_PATH
+$GIT clone https://github.com/pass-wall/passwall-desktop $DESKTOP_PATH
+$GIT clone https://github.com/pass-wall/passwall-web $WEB_PATH
+
+echo "Building passwall-server..."
+cd $SERVER_PATH && CGO_ENABLED=1 GOOS=linux $GO build -a --ldflags="-s" -o passwall-server
+cp $SERVER_PATH/passwall-server $SERVER_BIN_PATH/passwall-server
+cp -r $SERVER_PATH/store $SERVER_BIN_PATH/store
+echo "Building passwall-server completed successfully."
+
+echo "Building passwall-web..."
+sed -i "s/next build/next build \&\& next export/g" $WEB_PATH/package.json
+cd $WEB_PATH && $NPM install && $NPM run build
+cp -r $WEB_PATH/out/* $WEB_BIN_PATH/
+echo "Building passwall-web completed successfully."
+
+echo "Building passwall-desktop..."
+cd $DESKTOP_PATH && $NPM install && $NPM run build:linux
+cp -r $DESKTOP_PATH/build/PassWall-linux-x64/* $DESKTOP_BIN_PATH/
+echo "Building passwall-desktop completed successfully."
+
+echo "Building deb package..."
+dpkg-deb --build $DEBIAN_PATH
+echo "Building deb package completed successfully."
+
+echo "You can install this deb package by running: sudo apt install $DEBIAN_PATH.deb"
